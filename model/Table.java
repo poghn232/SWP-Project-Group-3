ae1231fc437bc91bf473c90245959f2410df5f63
poghn232 <vanphongnguyen486@gmail.com>
2025-05-22 23:48:58 +0700

    Table model

---
+++ model/Table.java
@@ -0,0 +1,45 @@
+public class Table{
+   private int tableId;
+   private String tableNumber;
+   private int capacity;
+   private String status;
+
+   public Table(int tableId, String tableNumber, int capacity, String status) {
+    this.tableId = tableId;
+    this.tableNumber = tableNumber;
+    this.capacity = capacity;
+    this.status = status;
+   }
+
+   public int getTableId() {
+    return tableId;
+   }
+
+   public void setTableId(int tableId) {
+    this.tableId = tableId;
+   }
+
+   public String getTableNumber() {
+    return tableNumber;
+   }
+
+   public void setTableNumber(String tableNumber) {
+    this.tableNumber = tableNumber;
+   }
+
+   public int getCapacity() {
+    return capacity;
+   }
+
+   public void setCapacity(int capacity) {
+    this.capacity = capacity;
+   }
+
+   public String getStatus() {
+    return status;
+   }
+
+   public void setStatus(String status) {
+    this.status = status;
+   }
+}
