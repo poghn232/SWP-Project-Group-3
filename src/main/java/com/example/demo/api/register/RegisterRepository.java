23ff5d887864f32f156c733ca4c2ea4d52c9b01c
Nguyễn Văn Phong <vanphongnguyen486@gmail.com>
2025-06-01 22:57:53 +0700

    Register Function (cont) - Study SpringBoot and fixing bugs. Commented files as unfinished codes

--- src/main/java/com/example/demo/api/register/RegisterRepository.java
+++ src/main/java/com/example/demo/api/register/RegisterRepository.java
@@ -1,8 +1,8 @@
-package com.example.demo.api.register;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegisterRepository extends JpaRepository<UserRegisterDto, String> {
-}
+//package com.example.demo.api.register;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface RegisterRepository extends JpaRepository<UserRegisterDto, String> {
+//}
