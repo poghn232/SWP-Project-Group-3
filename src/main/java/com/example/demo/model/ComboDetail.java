42ad15f7d8ba9ed2076eda3702426c534ecd8a24
quanpmhe163950 <quanpmhe163950@fpt.edu.vn>
2025-06-13 08:00:36 +0700

    fix table match name

--- src/main/java/com/example/demo/model/ComboDetail.java
+++ src/main/java/com/example/demo/model/ComboDetail.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "combo_details")
+@Table(name = "combodetail")
 public class ComboDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
